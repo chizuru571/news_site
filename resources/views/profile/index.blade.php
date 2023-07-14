1ea5109538b76fd35aa1813c5644d5ea2b8dad68
chizuru <rcbu7410@gmail.com>
2023-07-14 05:17:11 +0000

    indexファイルの変更

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,11 +9,6 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ secure_asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
                                 <div class="title p-2">
                                     <h1>{{ Str::limit($headline->title, 70) }}</h1>
                                 </div>
@@ -37,16 +32,11 @@
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
                                 <div class="title">
-                                    {{ Str::limit($post->title, 150) }}
+                                    {{ Str::limit($post->name, 150) }}
                                 </div>
                                 <div class="body mt-3">
-                                    {{ Str::limit($post->body, 1500) }}
+                                    {{ Str::limit($post->introduction, 1500) }}
                                 </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ secure_asset('storage/image/' . $post->image_path) }}">
-                                @endif
                             </div>
                         </div>
                     </div>
